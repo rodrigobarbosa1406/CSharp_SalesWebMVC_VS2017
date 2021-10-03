8ee55870c891caabfd21f4a7f88b79b988fe24f5
rodrigobarbosa1406 <rodrigobarbosanb@hotmail.com>
2021-10-03 17:40:23 -0300

    Seeding Service

---
+++ SalesWebMVC/Migrations/20211003203907_OtherEntities_v2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMVC.Migrations
+{
+    public partial class OtherEntities_v2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "SalesRecord");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "SalesRecord",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "SalesRecord");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                table: "SalesRecord",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
